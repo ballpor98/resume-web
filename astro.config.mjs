fc3f6b2335f0a16ca3ab206ac4281cad37bffee3
Chawin Kasikitpongpan <chawin.k@kbtg.tech>
2022-10-14 22:32:01 +0700

    build: update package and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
   integrations: [mdx(), sitemap(), tailwind()]
 });
